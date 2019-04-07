a1b0e029c0b3f74701fda6173c7fd334b098b787
Bishal Lamsal <bishalrajlamsal65@gmail.com>
2019-04-07 16:43:15 +0545

    first Post of Nepal safari

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router ,Route} from 'react-router-dom';
+import Home from './component/Home';
+import About from './component/About';
+import Navbar from './component/NavBar';
+import AdminHome from './admin/HomeAdmin';
+import PackagePage from './admin/PackagePage';
+import PackageDetail from './admin/PackageDetail';
+import BookPage from './admin/Book';
+import SignUp from './admin/Signup';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+       <Router>
+         
+         <div>
+         <Navbar/>
+           <Route exact path="/" component={Home}/>
+           <Route path="/about" component={About}/>
+           <Route path="/admin" component={AdminHome}/>
+           <Route path="/package" component={PackagePage}/>
+           <Route path="/packagedetail" component={PackageDetail} />
+           <Route path="/signup" component={SignUp} />
+           <Route path="/book" component={BookPage} />
+         </div>
+       </Router>
+        
       </div>
     );
   }
