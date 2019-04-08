194a796b2c647fc843277338f01432dcd7e5982a
Bishal Lamsal <bishalrajlamsal65@gmail.com>
2019-04-08 07:00:06 +0545

    Updates

---
+++ precache-manifest.a080daa864f354bf0f547e11bdad359a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nepal-safari/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5065302a35f96ae89722",
+    "url": "/nepal-safari/static/js/runtime~main.a33e72af.js"
+  },
+  {
+    "revision": "70eae599e9e8610380e6",
+    "url": "/nepal-safari/static/js/main.cbeaabc7.chunk.js"
+  },
+  {
+    "revision": "e35c2b479c4846b439fe",
+    "url": "/nepal-safari/static/js/2.1712789a.chunk.js"
+  },
+  {
+    "revision": "70eae599e9e8610380e6",
+    "url": "/nepal-safari/static/css/main.543c7076.chunk.css"
+  },
+  {
+    "revision": "11576c5ef1d455fce76f793d34e3f537",
+    "url": "/nepal-safari/index.html"
+  }
+];
